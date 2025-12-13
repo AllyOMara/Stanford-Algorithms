699f6b9bc28c3a9baa08dae5c1f74e5a796f6503
AllyOMara <allystellaomara@gmail.com>
2025-12-13 16:01:32 +1100

    Add inverse counter
    - Counts number of inverses

--- Part_1/Module_2_Assignment.py
+++ Part_1/Module_2_Assignment.py
@@ -1,4 +1,9 @@
 '''
+[Stanford Algorithms - Part 1...]
+Programming Assignment
+
+Solution by Alexandria O'Mara
+
 TASK
 
 This file (integerarray.txt) contains all of the 100,000 integers between
@@ -11,13 +16,20 @@ divide-and-conquer algorithm covered in the video lectures.
 
 import time
 
-inv_count   = 0
-input_array = []
-start_time  = time.perf_counter()
+####################################################################
+########################## GLOBALS #################################
+####################################################################
+
+inverse_count   = 0
+start_time      = time.perf_counter()
+
+####################################################################
+########################## FUNCTIONS ###############################
+####################################################################
 
 '''
-Count left and right inversions.
-Also sort the left and right halves (to achieve O(nlogn))
+Merge Sort
+Count left and right inversions
 '''
 def merge_and_count(array):
 
@@ -34,16 +46,15 @@ def merge_and_count(array):
         right = merge_and_count(right)
 
     merged = []
-    global inv_count
+    global inverse_count
 
     while len(left) > 0 and len(right) > 0:
         if left[0] > right[0]:
+            inverse_count = inverse_count + len(left)
             merged.append(right[0])
             right.pop(0)
-            inv_count = inv_count + len(left) # UNSURE
         elif left[0] < right[0]:
             merged.append(left[0])
-            inv_count = inv_count + len(right)
             left.pop(0)
 
     if len(right) > 0:
@@ -53,23 +64,28 @@ def merge_and_count(array):
 
     return merged
 
-#################################################################################
+####################################################################
+########################### EXECUTE ################################
+####################################################################
 
 file_name_1 = "integerarray.txt"    # Assigned file
-file_name_2 = "even.txt"            # File with an even number integers
-file_name_3 = "odd.txt"             # File with an odd number of integers
+file_name_2 = "even.txt"            # File with an even number integers. Has 7 inverses.
+file_name_3 = "odd.txt"             # File with an odd number of integers. Has 6 inverses.
 file_name_4 = "36_inv.txt"          # File with 36 inverses
+file_name_5 = "2_inv.txt"           # File with 2 inverses
+file_name_6 = "0_inv.txt"           # File with 0 inverses (i.e. sorted).
 
-# Read all lines in the file and place in an array
-with open(file_name_4) as file:
+# Read data from file
+input_array = []
+with open(file_name_1) as file:
     for line in file:
         input_array.append(int(line))
 len_input = len(input_array)
 
-sorted_array    = merge_and_count(input_array)
-end_time        = time.perf_counter()
-elapsed_time    = end_time - start_time
+sorted_array = merge_and_count(input_array)
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
 
 print(f"The sorted array is: {sorted_array}.")
-print(f"The number of inverses present in the file is: {inv_count}.")
+print(f"The number of inverses present in the file is: {inverse_count}.")
 print(f"The time it took to sort {len_input} items and count the number of inverses was: {elapsed_time} seconds.")
