a615c3c00386495358d2ffefd538b3b794ebe5cf
AllyOMara <allystellaomara@gmail.com>
2025-12-15 13:10:33 +1100

    Update formatting
    - Delete unnecessary comments from scaffolding
    - Delete unnecessary line breaks
    - Align some equal signs (where necessary)

--- Part_1/Module_3_Assignment_Part_1.py
+++ Part_1/Module_3_Assignment_Part_1.py
@@ -19,39 +19,36 @@ For the first part of the programming assignment, you should
 always use the first element of the array as the pivot element.
 '''
 
-####################################################################
 ########################## GLOBALS #################################
-####################################################################
 
 comparisons = 0     # Count of performed comparisons
 
-####################################################################
 ########################## FUNCTIONS ###############################
-####################################################################
 
 '''
 Quick Sort
 Count number of comparisons
 '''
 def quick_sort(array):
-    # Return if the length of the array is 1
+    
     len_array = len(array)
     if len(array) == 1:
         return array
-    # Partition A around P (i.e. get all <p to the right and all >p to the left)
+    
+    # Partition
     i       = 1   # index where elements less than the pivot ends
-    pivot = [array[0]]
+    pivot   = [array[0]]
 
     for j in range(1, len_array):
         if array[0] > array[j]:
             array[i], array[j] = array[j], array[i]
             i = i + 1
-    array[i - 1], array[0] = array[0], array[i - 1] # "Puts" pivot in place
+    
+    array[i - 1], array[0]  = array[0], array[i - 1] # "Puts" pivot in place
+    left                    = array[:i - 1]
+    right                   = array[i:]
 
-    left    = array[:i - 1]
-    right   = array[i:]
-
-    # Recursively sort left and right
+    # Recursion
     if len(left) > 1:
         left = quick_sort(left)
     if len(right) > 1:
@@ -62,14 +59,7 @@ def quick_sort(array):
     
     return(array)
 
-
-
-
-
-
-####################################################################
 ########################### EXECUTE ################################
-####################################################################
 
 file_name_1 = "QuickSort.txt"       # Assigned file
 file_name_2 = "1_to_10.txt"         # File with numbers 1 to 10 in a random order
