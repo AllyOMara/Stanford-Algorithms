52bcc393b240c9cb4a68f24b77a0ad5aedad5dd5
AllyOMara <allystellaomara@gmail.com>
2025-12-15 16:47:17 +1100

    Add Module_3_Assignment_Part_3.py

---
+++ Part_1/Module_3_Assignment_Part_3.py
@@ -0,0 +1,24 @@
+'''
+Stanford Algorithms - Part 1 Module 2
+Programming Assignment
+
+Solution by Alexandria O'Mara
+
+TASK
+
+This file (QuickSort.txt) contains all of the integers between
+1 and 10,000 (inclusive, with no repeats) in unsorted order.
+The integer in the ith row of the file gives you the ith entry
+of an input array.
+Compute the total number of comparisons used to sort the given
+input file by QuickSort. As you know, the number of comparisons
+depends on which elements are chosen as pivots, so we'll ask
+you to explore three different pivoting rules
+
+Compute the number of comparisons (as in Problem 1), using the
+"median-of-three" pivot rule. I.e. Choose the pivot as follows.
+Consider the first, middle, and final elements of the given array.
+Identify which of these three elements is the median, and use this
+as your pivot.
+'''
+
