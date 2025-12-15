464679ecbc190882ccb75a8fc190e9e3a8f4e914
AllyOMara <allystellaomara@gmail.com>
2025-12-15 16:48:15 +1100

    Update Module_3_Assignment_Part_2.py

---
+++ Part_1/Module_3_Assignment_Part_2.py
@@ -0,0 +1,93 @@
+'''
+Stanford Algorithms - Part 1 Module 2
+Programming Assignment
+
+Solution by Alexandria O'Mara
+
+TASK
+
+This file (QuickSort.txt) contains all of the integers between
+1 and 10,000 (inclusive, with no repeats) in unsorted order.
+The integer in the ith row of the file gives you the ith entry
+of an input array.
+Compute the total number of comparisons used to sort the given
+input file by QuickSort. As you know, the number of comparisons
+depends on which elements are chosen as pivots, so we'll ask
+you to explore three different pivoting rules
+
+Just before the main Partition subroutine, you should exchange
+the pivot element (i.e., the last element) with the first element.
+
+For the second part of the programming assignment, you should
+always use the last element of the array as the pivot element.
+'''
+
+import time
+
+############################# GLOBALS ##############################
+
+comparisons = 0     # Count of performed comparisons
+
+############################ FUNCTIONS #############################
+
+'''
+Quick Sort
+Count number of comparisons
+'''
+def quick_sort(array):
+    
+    len_array = len(array)
+    if len_array == 1:
+        return array
+    
+    # Partition
+    i           = 1   # Index where elements less than the pivot ends
+    last_index  = len_array - 1
+    pivot       = [array[len_array - 1]]
+    array[last_index], array[0]  = array[0], array[last_index] # Swaps pivot element with the first element to mainatain consistency
+
+    global comparisons
+    comparisons = comparisons + len_array - 1
+
+    for j in range(1, len_array):
+        if array[0] > array[j]:
+            array[i], array[j] = array[j], array[i]
+            i = i + 1
+    
+    array[i - 1], array[0]  = array[0], array[i - 1] # "Puts" pivot in place
+    left                    = array[:i - 1]
+    right                   = array[i:]
+
+    # Recursion
+    if len(left) > 1:
+        left = quick_sort(left)
+    if len(right) > 1:
+        right = quick_sort(right)
+
+    # Combine into final array
+    array = left + pivot + right
+    
+    return(array)
+
+########################### EXECUTE ################################
+
+file_name_1 = "QuickSort.txt"       # Assigned file
+file_name_2 = "1_to_10.txt"         # File with numbers 1 to 10 (inclusive) in a random order
+file_name_3 = "integerarray.txt"    # File with numbers 1 to 100000 (inclusive) in a random order
+
+
+# Read data from file
+input_array = []
+with open(file_name_1) as file:
+    for line in file:
+        input_array.append(int(line))
+len_input = len(input_array)
+
+start_time = time.perf_counter()
+sorted_array = quick_sort(input_array)
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+
+print(f"The sorted array is: {sorted_array}.")
+print(f"The number of comparisons made was: {comparisons}.")
+print(f"The time it took to sort {len_input} items and count the number of inverses was: {elapsed_time} seconds.")
