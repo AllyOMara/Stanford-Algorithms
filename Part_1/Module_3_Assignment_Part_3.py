da0ea0d01e4c0d4ef306662f61f52b0ddf94fa70
AllyOMara <allystellaomara@gmail.com>
2025-12-15 18:16:05 +1100

    Update Module_3_Assignment_Part_3.py
    - Add quick_sort (uses quick sort to sort an array from smallest to largest)
    - Add find_median (for three elements)

--- Part_1/Module_3_Assignment_Part_3.py
+++ Part_1/Module_3_Assignment_Part_3.py
@@ -13,7 +13,7 @@ of an input array.
 Compute the total number of comparisons used to sort the given
 input file by QuickSort. As you know, the number of comparisons
 depends on which elements are chosen as pivots, so we'll ask
-you to explore three different pivoting rules
+you to explore three different pivoting rules.
 
 Compute the number of comparisons (as in Problem 1), using the
 "median-of-three" pivot rule. I.e. Choose the pivot as follows.
@@ -22,3 +22,102 @@ Identify which of these three elements is the median, and use this
 as your pivot.
 '''
 
+import time
+
+########################## GLOBALS #################################
+
+comparisons = 0     # Count of performed comparisons
+
+########################## FUNCTIONS ###############################
+
+'''
+Find the median value from a list of three values
+Returns the median value
+'''
+def find_median(first, middle, last):
+    min_value = min(first, middle, last)
+    max_value = max(first, middle, last)
+    if first != min_value and first != max_value:
+        return first
+    elif middle != min_value and middle != max_value:
+        return middle
+    else:
+        return last
+
+'''
+Quick Sort
+Count number of comparisons
+'''
+def quick_sort(array):
+    
+    len_array = len(array)
+    if len_array == 1:
+        return array
+    
+    first_index   = 0
+    middle_index  = len_array // 2
+    last_index    = len_array - 1
+
+    first_value   = array[first_index]
+    middle_value  = array[middle_index]
+    last_value    = array[last_index]
+    
+    median_value = find_median(first_value, middle_value, last_value)
+
+    if median_value == first_value:
+        pivot_index = first_index
+    elif median_value == middle_value:
+        pivot_index = middle_index
+    else:
+        pivot_index = last_index
+
+    # Partition
+    i           = 1   # Index where elements less than the pivot ends
+    pivot_array = [array[pivot_index]]
+    array[pivot_index], array[0]  = array[0], array[pivot_index] # Swaps pivot element with the first element to mainatain consistency
+
+    global comparisons
+    comparisons = comparisons + len_array - 1
+
+    for j in range(1, len_array):
+        if array[0] > array[j]:
+            array[i], array[j] = array[j], array[i]
+            i = i + 1
+    
+    array[i - 1], array[0]  = array[0], array[i - 1] # "Puts" pivot in place
+    left                    = array[:i - 1]
+    right                   = array[i:]
+
+    # Recursion
+    if len(left) > 1:
+        left = quick_sort(left)
+    if len(right) > 1:
+        right = quick_sort(right)
+
+    # Combine into final array
+    array = left + pivot_array + right
+    
+    return(array)
+
+########################### EXECUTE ################################
+
+file_name_1 = "QuickSort.txt"       # Assigned file
+file_name_2 = "1_to_10.txt"         # File with numbers 1 to 10 (inclusive) in a random order
+file_name_3 = "integerarray.txt"    # File with numbers 1 to 100000 (inclusive) in a random order
+
+
+# Read data from file
+input_array = []
+with open(file_name_1) as file:
+    for line in file:
+        input_array.append(int(line))
+len_input = len(input_array)
+
+start_time = time.perf_counter()
+sorted_array = quick_sort(input_array)
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+
+print(f"The sorted array is: {sorted_array}.")
+print(f"The number of comparisons made was: {comparisons}.")
+print(f"The time it took to sort {len_input} items and count the number of inverses was: {elapsed_time} seconds.")
