ce1cba72cc1c04999eb6c83669b8a023464944f9
AllyOMara <allystellaomara@gmail.com>
2025-12-22 14:29:18 +1100

    Add instructions

---
+++ Part_2/Module_1_Assignment.py
@@ -0,0 +1,31 @@
+'''
+Stanford Algorithms - Part 2 Module 1
+Programming Assignment
+
+Solution by Alexandria O'Mara
+
+TASK
+
+This file (SCC.txt) contains the edges of a directed graph.
+Vertices are labeled as positive integers from 1 to 875714.
+Every row indicates an edge, the vertex label in first
+column is the tail and the vertex label in second column is
+the head.
+
+Your task is to code up the algorithm from the video
+lectures for computing strongly connected components (SCCs),
+and to run this algorithm on the given graph.
+
+Output Format: You should output the sizes of the 5 largest
+SCCs in the given graph, in decreasing order of sizes,
+separated by commas (avoid any spaces). If your algorithm
+finds less than 5 SCCs, then write 0 for the remaining terms.
+'''
+
+import time
+
+########################## GLOBALS #################################
+
+SCCs = [0, 0, 0, 0, 0]     # List of the sizes of found SCCs
+
+########################## FUNCTIONS ###############################
