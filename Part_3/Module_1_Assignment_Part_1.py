d0caf979b9376002c5354caa06f11bc410e5245b
Ally <124349321+AllyOMara@users.noreply.github.com>
2026-01-09 15:10:41 +1100

    Update and fill out functions
    * Update docstrings of:
        * find_median(first, middle, last)
        * quick_sort(array)
        * greedy()
    
    * Fill out functions:
        * calculate_schedule(key_to_job_dict, (etc.))
        * calculate_completion_time(job_schedule, weights, lengths)
    
    * Update functions:
        * quick_sort(array) - comparisons variable removed (unnecessary)
        * calculate_job_keys(file_name) - remove unnecessary print
        * create_key_to_job_dict(job_keys) - fix indexing
        * create_weights_list(file_name) - fix indexing
        * calculate_schedule(key_to_job_dict, (etc.)) - fill out more of the function
        * greedy() - add testing file, call several functions, print job_schedule and completion_time
    
    * Change function name from create_key_to_job_dictionary to create_key_to_job_dict
    
    * Create create_lengths_list(file_name)
    
    * Update TO DO list

--- Part_3/Module_1_Assignment_Part_1.py
+++ Part_3/Module_1_Assignment_Part_1.py
@@ -33,9 +33,9 @@ def find_median(first, middle, last):
     :param middle: Integer (middle element of an array).
     :param last: Integer (last element of an array).
     Returns:
-        The median of the three inputted integers.
+    The median of the three inputted integers.
     """
-  
+
     min_value = min(first, middle, last)
     max_value = max(first, middle, last)
     if first != min_value and first != max_value:
@@ -47,10 +47,10 @@ def find_median(first, middle, last):
 
 
 def quick_sort(array):
-    """ Sorts an inputted array and counts the total number of comparisons made between elements.
-    :param array: Array containing integers and no duplicates in an arbitrary order.
+    """ Sorts an inputted array.
+    :param array: Array containing integers and no duplicates.
     Returns:
-        array in non-decreasing order.
+    Array in non-decreasing order.
     """
 
     len_array = len(array)
@@ -81,9 +81,6 @@ def quick_sort(array):
     i = 1   # Index where elements less than the pivot ends
     pivot_array = [array[pivot_index]]
     array[pivot_index], array[0] = array[0], array[pivot_index] # Swaps pivot element with the first element
-
-    global comparisons
-    comparisons = comparisons + len_array - 1
 
     for j in range(1, len_array):
         if array[0] > array[j]:
@@ -118,7 +115,6 @@ def calculate_job_keys(file_name):
         for line in file:
             job_description = line.split()
             if len(job_description) == 2:
-                print(job_description)
                 job_weight = int(job_description[0])
                 job_length = int(job_description[1])
                 job_key = int(job_weight - job_length)
@@ -126,7 +122,7 @@ def calculate_job_keys(file_name):
     return job_keys
 
 
-def create_key_to_job_dictionary(job_keys):
+def create_key_to_job_dict(job_keys):
     """ Loop through job_keys (array), adding the job associated with each key
     to a dictionary.
 
@@ -139,9 +135,9 @@ def create_key_to_job_dictionary(job_keys):
     for i in range(len(job_keys)):
         key = job_keys[i]
         if key not in dictionary:
-            dictionary.update({key : [i]})
+            dictionary.update({key : [i + 1]})
         else:
-            dictionary[key].append(i)
+            dictionary[key].append(i + 1)
     
     return dictionary
             
@@ -160,29 +156,79 @@ def create_weights_list(file_name):
             job_description = line.split()
             if len(job_description) > 1:
                 weights_list.append(int(job_description[0]))
+            else:
+                weights_list.append(0)
     return weights_list
 
 
-def calculate_final_schedule(key_to_job_dictionary, weights_list, sorted_keys):
+def create_lengths_list(file_name):
+    """ List of lengts of all jobs. Returned in an array.
+    Used to calculate completion times.
+    """
+
+    lengths_list = []
+    with open(file_name) as file:
+        for line in file:
+            job_description = line.split()
+            if len(job_description) > 1:
+                lengths_list.append(int(job_description[1]))
+            else:
+                lengths_list.append(0)
+    return lengths_list
+            
+
+
+def calculate_schedule(key_to_job_dict,
+                       weights_list,
+                       sorted_keys):
     """ Returns the final schedule of jobs based on their keys.
     """
 
     visited_keys = []
     final_schedule = []
-    for i in range(len(sorted_keys)):
-        key = sorted_keys[i]
-    pass
+    while len(sorted_keys) > 0:
+        key_index = len(sorted_keys) - 1
+        key = sorted_keys[key_index]
+        sorted_keys.pop(key_index)
+        if key not in visited_keys:
+            jobs = key_to_job_dict[key]
+            if len(jobs) == 1:
+                final_schedule.append(jobs[0])
+            else:
+                while len(jobs) > 0:
+                    for i in range(len(jobs)):
+                        possible_job_weight = weights_list[i]
+                        if i == 0:
+                            job = jobs[i]
+                            job_index = 0
+                            job_weight = possible_job_weight
+                        else:
+                            if possible_job_weight >= job_weight:
+                                job = jobs[i]
+                                job_index = i
+                                job_weight = possible_job_weight
+                    final_schedule.append(job)
+                    jobs.pop(job_index)
+    return final_schedule
 
 
-
-
-def calculate_completion_time(final_schedule):
+def calculate_completion_time(job_schedule, weights, lengths):
     """ Returns the final answer (the sum of weighted completion times).
 
     Uses final_schedule to calculate the sum of weighted completion times, which
     is returned.
     """
-    pass
+
+    completion_time = 0
+    sum_completion_times = 0
+    for i in range(len(job_schedule)):
+        job = job_schedule[i]
+        job_weight = weights[job]
+        job_length = lengths[job]
+        completion_time = completion_time + job_length
+        weighted_completion_time = job_weight * completion_time
+        sum_completion_times = sum_completion_times + weighted_completion_time
+    return sum_completion_times
 
 
 def greedy():
@@ -192,16 +238,23 @@ def greedy():
     jobs in a close-to-optimal ordering. Prints final schedule.
     If there is a clash between keys, choose the job with the highest weight 
     before the job of the lowest weight.
-    **IMPORTANT NOTE**: add a case where the weights are equal.
     """
     
     FILE_NAME_1 = 'jobs.txt'
-    chosen_file = FILE_NAME_1
+    FILE_NAME_2 = 'jobs_test_1.txt' # Ordering: 2,8,7,1,5,4,3,6
+    
+    chosen_file = FILE_NAME_2
 
     job_keys = calculate_job_keys(chosen_file)
-    key_to_job_dicitonary = create_key_to_job_dictionary(job_keys)
-    weight_list = create_weights_list(chosen_file)
+    key_to_job_dict = create_key_to_job_dict(job_keys)
+    weights = create_weights_list(chosen_file)
+    lengths = create_lengths_list(chosen_file)
     sorted_keys = quick_sort(job_keys)
+    job_schedule = calculate_schedule(key_to_job_dict, weights, sorted_keys)
+    print(job_schedule)
+    completion_time = calculate_completion_time(job_schedule, weights, lengths)
+
+    print(completion_time)
 
 
 def main():
@@ -216,11 +269,11 @@ if __name__ == "__main__":
 TO DO:
 
 x. Fill in functions
-x. Make small test files
 x. Test on small test files
 
 Completed:
 x. Make outline ("skeleton")
+x. Make small test files
 
 
 '''
