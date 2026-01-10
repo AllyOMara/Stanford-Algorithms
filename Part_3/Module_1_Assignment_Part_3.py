377bcd91286c02665af51bb15f920f499b992f17
AllyOMara <allystellaomara@gmail.com>
2026-01-10 12:52:12 +1100

    Fill out functions
    Fill out functions:
    * create_graph(file_name)
    * create_visited_nodes(max_range)
    * create_edge_costs(file_name)
    * prim() --- ONLY PART

--- Part_3/Module_1_Assignment_Part_3.py
+++ Part_3/Module_1_Assignment_Part_3.py
@@ -27,21 +27,37 @@ cost of a minimum spanning tree.
 '''
 
 
-def create_graph(file_name):
+def create_graph(file_name, max_range):
     """ Creates an adjacency list of where each node is connected to.
     """
+    
+    graph = [[]]
+    for i in range(max_range):
+        graph.append([])
+    with open(file_name) as file:
+        for line in file:
+            edge_description = line.split()
+            if len(edge_description) > 1:
+                first_node = edge_description[0]
+                second_node = edge_description[1]
+                graph[first_node].append(second_node)
+                graph[second_node].append(first_node)   # Added twice due to the undirected nature of the graph
+    return graph
 
 
-def create_visited_nodes():
+def create_visited_nodes(max_range):
     """ Creates list of size max node value containing False (to be updated
     when a node is visited)
     """
+    visited_nodes = [True]  # No node '0'
+    for i in range(max_range):
+        visited_nodes.append(False)
 
 
 def create_edge_costs(file_name):
     """ Creates dictionary of edge costs between nodes.
 
-    Will look something like this:
+    Should look something like this:
     edge_costs = { 
                     1:  1: 3,
                         2: 5,
@@ -50,8 +66,40 @@ def create_edge_costs(file_name):
                         6: 7
                  }
     """
+    edge_costs = {}
+    with open(file_name) as file:
+        for line in file:
+            edge_description = line.split()
+            if len(edge_description) > 1:
+                first_node = edge_description[0]
+                second_node = edge_description[1]
+                edge_cost = edge_description[2]
+                if first_node not in edge_costs:
+                    edge_costs.update({first_node: {second_node : edge_cost}})
+                else:
+                    edge_costs[first_node].update({second_node: edge_cost})
+                if second_node not in edge_costs:
+                    edge_costs.update({second_node: {first_node: edge_cost}})
+                else:
+                    edge_costs[second_node].update({first_node: edge_cost})
+    return edge_costs
 
 
 def prim():
     """ Uses Prim's Algorithm to calculate the minimum cost spanning tree
     """
+    FILE_NAME_1 = 'edges.txt'   # Assigned file, MAX_RANGE = 500
+    MAX_RANGE = 500
+    chosen_file = FILE_NAME_1
+
+    create_graph(FILE_NAME_1, MAX_RANGE)
+    create_visited_nodes(MAX_RANGE)
+    create_edge_costs(chosen_file)
+
+
+def main():
+    prim()
+
+
+if __name__ == "__main__":
+    main()
