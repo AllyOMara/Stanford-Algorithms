23cad1014ad96f72077579810b0a99ccf7ef2d5f
AllyOMara <allystellaomara@gmail.com>
2026-01-15 16:36:25 +1100

    Add task details

---
+++ Part_3/Module_4_Assignment_Part_1.py
@@ -0,0 +1,19 @@
+"""
+Stanford Algorithms - Part 3 Module 4
+Programming Assignment
+
+Solution by Alexandria O'Mara
+
+TASK
+
+This file describes a knapsack instance, and it has the following format:
+[knapsack_size][number_of_items]
+[value_1] [weight_1]
+[value_2] [weight_2]
+...
+For example, the third line of the file is "50074 659", indicating that the
+second item has value 50074 and size 659, respectively.
+
+What is the value of the optimal solution?
+"""
+
