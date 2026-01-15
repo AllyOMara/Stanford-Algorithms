0a77c8f33ea9ff71106671c3d136e3683e630ee9
AllyOMara <allystellaomara@gmail.com>
2026-01-16 10:30:20 +1100

    Fix and fill out functions
    * Fix functions:
        * create_item_values
        * create_item_weights
    
    * Fill out functions:
        * find_knapsack_size
        * find_max_items
        * create_knapsack_array
        * knapsack (*NOT FINISHED*)

--- Part_3/Module_4_Assignment_Part_1.py
+++ Part_3/Module_4_Assignment_Part_1.py
@@ -23,15 +23,15 @@ def create_item_values(file_name):
     """ Creates and returns array of all item values.
     """
 
-    values = []
+    values = [0]  # Better indexing
     first_line = True
     with open(file_name) as file:
         for line in file:
             if first_line:
                 first_line = False
             else:
-                item_description = line.split
-                item_value = item_description[0]
+                item_description = line.split()
+                item_value = int(item_description[0])
                 values.append(item_value)
     return values
 
@@ -40,15 +40,15 @@ def create_item_weights(file_name):
     """ Creates and returns array of all item weights.
     """
     
-    weights = []
+    weights = [0]  # Better indexing
     first_line = True
     with open(file_name) as file:
         for line in file:
             if first_line:
                 first_line = False
             else:
-                item_description = line.split
-                item_weight = item_description[0]
+                item_description = line.split()
+                item_weight = int(item_description[0])
                 weights.append(item_weight)
     return weights
 
@@ -57,16 +57,44 @@ def find_knapsack_size(file_name):
     """ Finds and returns knapsack size.
     """
 
+    first_line = True
+    with open(file_name) as file:
+        for line in file:
+            if first_line:
+                knapsack_description = line.split()
+                knapsack_size = int(knapsack_description[0])
+                first_line = False
+    return knapsack_size
+
 
 def find_max_items(file_name):
-      """ Finds and returns the maximum number of items.
-    """  
+    """ Finds and returns the maximum number of items.
+    """
+
+    first_line = True
+    with open(file_name) as file:
+        for line in file:
+            if first_line:
+                knapsack_description = line.split()
+                max_items = int(knapsack_description[1])
+                first_line = False
+    return max_items
 
 
 def create_knapsack_array(max_items, knapsack_size):
     """ Creates a 2D array of size max_items containing arrays of size knapsack_size.
     - Also initialises A[0][x] = 0 for all x = 0, 1, W
     """
+    
+    knapsack_array = []
+    for i in range(max_items):
+        knapsack_array.append([])
+    for i in range(max_items):
+        for j in range(knapsack_size):
+            knapsack_array[i].append(None)
+    for i in range(knapsack_size):
+        knapsack_array[0][i] = 0
+    return(knapsack_array)
 
 
 def find_knapsack_value(knapsack_array, item_values, item_weights, knapsack_size):
@@ -77,6 +105,16 @@ def find_knapsack_value(knapsack_array, item_values, item_weights, knapsack_size
 def knapsack():
     """ Knapsack algorithm used to find the optimal solution.
     """
+    
+    FILE = "knapsack1.txt"  # Assigned file
+    
+    values = create_item_values(FILE)
+    weights = create_item_weights(FILE)
+    knapsack_size = find_knapsack_size(FILE)
+    max_items = find_max_items(FILE)
+    knapsack_array = create_knapsack_array(max_items, knapsack_size)
+    print(values, weights, knapsack_size, max_items)
+
 
 
 def main():
