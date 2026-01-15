9709271c1cbe50fb51cb63f476fdcc4c3df0e68a
AllyOMara <allystellaomara@gmail.com>
2026-01-15 16:46:52 +1100

    Add outline

--- Part_3/Module_4_Assignment_Part_1.py
+++ Part_3/Module_4_Assignment_Part_1.py
@@ -6,7 +6,8 @@ Solution by Alexandria O'Mara
 
 TASK
 
-This file describes a knapsack instance, and it has the following format:
+This file (knapsack1.txt) describes a knapsack instance.
+It has the following format:
 [knapsack_size][number_of_items]
 [value_1] [weight_1]
 [value_2] [weight_2]
@@ -17,3 +18,46 @@ second item has value 50074 and size 659, respectively.
 What is the value of the optimal solution?
 """
 
+
+def create_item_values(file_name):
+    """ Creates and returns array of all item values.
+    """
+
+
+def create_item_weights(file_name):
+    """ Creates and returns array of all item weights.
+    """
+
+
+def find_knapsack_size(file_name):
+    """ Finds and returns knapsack size.
+    """
+
+
+def find_max_items(file_name):
+      """ Finds and returns the maximum number of items.
+    """  
+
+
+def create_knapsack_array(max_items, knapsack_size):
+    """ Creates a 2D array of size max_items containing arrays of size knapsack_size.
+    - Also initialises A[0][x] = 0 for all x = 0, 1, W
+    """
+
+
+def find_knapsack_value(knapsack_array, item_values, item_weights):
+    """ Finds and returns the value of the optimal solution.
+    """
+
+
+def knapsack():
+    """ Knapsack dynamic programming algorithm used to find the optimal solution to the knapsack problem.
+    """
+
+
+def main():
+    knapsack()
+
+
+if __name__ == "__main__":
+    main()
