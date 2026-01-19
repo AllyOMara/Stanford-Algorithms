4b9e7d691f0e2bab03911795f247f5585d370094
AllyOMara <allystellaomara@gmail.com>
2026-01-20 10:07:09 +1100

    Create Week_l_Assignment.py

---
+++ Course_4/Week_1_Assignment.py
@@ -0,0 +1,17 @@
+"""
+Stanford Algorithms - Course 4 Week 1
+Programming Assignment
+
+Solution by Alexandria O'Mara
+
+TASK
+
+In this assignment you will implement one or more algorithms for the all-pairs
+shortest-path problem. Three data files (g1.txt, g2.txt, and g3.txt) describing
+three graphs have the structure:
+[number_of_vertices] [number_of_edges]
+[first_vertex] [second_vertex] [edge_length]
+...
+
+
+"""
