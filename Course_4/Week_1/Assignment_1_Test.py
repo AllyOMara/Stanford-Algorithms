353e4d8094aec8182183f566fdf164e5920fac32
AllyOMara <allystellaomara@gmail.com>
2026-01-24 10:44:22 +1100

    Update Assignment_1_Test - simplify functions
    * Condense (simplify) functions to one function

--- Course_4/Week_1/Assignment_1_Test.py
+++ Course_4/Week_1/Assignment_1_Test.py
@@ -11,39 +11,24 @@ import Week_1_Assignment as week
 
 def test_1():
     file_name = "test_graph_1.txt"
-    edge_weights = week.create_edge_weight_dictionary(file_name)
-    number_of_nodes = week.get_number_of_nodes(file_name)
-    graph = week.create_adjacency_list(file_name, number_of_nodes)
-    shortest_paths_array = week.create_shortest_paths_array(number_of_nodes, graph, edge_weights)
-    answer = week.floyd_warshall(number_of_nodes, shortest_paths_array)
+    answer = week.find_shortest_shortest_path(file_name)
     assert answer == -41
 
 
 def test_2():
     file_name = "test_graph_2.txt"
-    edge_weights = week.create_edge_weight_dictionary(file_name)
-    number_of_nodes = week.get_number_of_nodes(file_name)
-    graph = week.create_adjacency_list(file_name, number_of_nodes)
-    shortest_paths_array = week.create_shortest_paths_array(number_of_nodes, graph, edge_weights)
-    answer = week.floyd_warshall(number_of_nodes, shortest_paths_array)
+    answer = week.find_shortest_shortest_path(file_name)
+
     assert answer == -208
 
 
 def test_3():
     file_name = "test_graph_3.txt"
-    edge_weights = week.create_edge_weight_dictionary(file_name)
-    number_of_nodes = week.get_number_of_nodes(file_name)
-    graph = week.create_adjacency_list(file_name, number_of_nodes)
-    shortest_paths_array = week.create_shortest_paths_array(number_of_nodes, graph, edge_weights)
-    answer = week.floyd_warshall(number_of_nodes, shortest_paths_array)
+    answer = week.find_shortest_shortest_path(file_name)
     assert answer == -431
 
 
 def test_4():
     file_name = "test_graph_4.txt"
-    edge_weights = week.create_edge_weight_dictionary(file_name)
-    number_of_nodes = week.get_number_of_nodes(file_name)
-    graph = week.create_adjacency_list(file_name, number_of_nodes)
-    shortest_paths_array = week.create_shortest_paths_array(number_of_nodes, graph, edge_weights)
-    answer = week.floyd_warshall(number_of_nodes, shortest_paths_array)
+    answer = week.find_shortest_shortest_path(file_name)
     assert answer == "NULL"
