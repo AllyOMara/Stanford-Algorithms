e2ebae7080c559a65ea252812fd27a9f47027d63
Ally <124349321+AllyOMara@users.noreply.github.com>
2026-01-27 16:08:04 +1100

    Update Assignment_Answer.sh

--- Course_4/Week_3/Assignment_Answer.sh
+++ Course_4/Week_3/Assignment_Answer.sh
@@ -2,11 +2,9 @@ python Assignment_3_Test.py
 passed_test=$?
 if [ $passed_test -eq 0 ]
 then
-echo "Tests passed."
-echo "Collecting answer..."
-python Week_1_Assignment.py g1.txt
-echo "Answer collected."
-echo "The answer is $answer."
+    echo "Tests passed."
+    echo "Collecting answer..."
+    python Week_1_Assignment.py g1.txt
 else
-echo "Tests failed."
+    echo "Tests failed."
 fi
