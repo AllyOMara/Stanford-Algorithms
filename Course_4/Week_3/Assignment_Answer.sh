88eb6ef5b54761adab4d407edea56c023d70835c
AllyOMara <allystellaomara@gmail.com>
2026-01-27 12:35:31 +1100

    Create shell script (for Linux)

---
+++ Course_4/Week_3/Assignment_Answer.sh
@@ -0,0 +1,12 @@
+python Assignment_3_Test.py
+passed_test=$?
+if [ $passed_test -eq 0 ]
+then
+echo "Tests passed."
+echo "Collecting answer..."
+answer=(python Week_1_Assignment.py g1.txt)
+echo "Answer collected."
+echo "The answer is $answer."
+else
+echo "Tests failed."
+fi
